029f5e7ba2299755d7cf2610f7b311daf68353c2
pan <pannanx@gmail.com>
2023-01-05 17:32:35 +0800

    refactor: 增加设备类型枚举

---
+++ payment-sdk/src/main/java/io/mvvm/halo/plugins/payment/sdk/DeviceType.java
@@ -0,0 +1,25 @@
+package io.mvvm.halo.plugins.payment.sdk;
+
+/**
+ * 设备类型.
+ *
+ * @author: pan
+ **/
+public enum DeviceType {
+    /**
+     * PC 端网页
+     */
+    pc,
+    /**
+     * 移动端网页
+     */
+    mobile,
+    /**
+     * 小程序
+     */
+    mini,
+    /**
+     * APP
+     */
+    app
+}
