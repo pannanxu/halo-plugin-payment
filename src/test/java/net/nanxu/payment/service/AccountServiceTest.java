4444b88599df819c90bf1098d9e880c743d2034d
Nanxu <pannanx@gmail.com>
2024-10-16 23:27:10 +0800

    commit files.

--- src/test/java/net/nanxu/payment/service/AccountServiceTest.java
+++ src/test/java/net/nanxu/payment/service/AccountServiceTest.java
@@ -3,9 +3,7 @@ package net.nanxu.payment.service;
 import java.util.List;
 import net.nanxu.payment.PaymentExtensionGetter;
 import net.nanxu.payment.ReactiveExtensionClientTest;
-import net.nanxu.payment.account.Account;
 import net.nanxu.payment.account.IAccount;
-import net.nanxu.payment.registry.AccountRegistry;
 import net.nanxu.payment.registry.PaymentRegistry;
 import net.nanxu.payment.service.impl.AccountServiceImpl;
 import net.nanxu.testplugin.WeChatPayment;
@@ -22,13 +20,10 @@ class AccountServiceTest {
 
     @BeforeEach
     void setUp() {
-        AccountRegistry accountRegistry = new AccountRegistry();
-        // accountRegistry.register(createMockAccount());
+        PaymentExtensionGetter getter = Mockito.mock(PaymentExtensionGetter.class);
+        Mockito.when(getter.getPaymentExtensions()).thenReturn(List.of(new WeChatPayment()));
 
-        PaymentExtensionGetter paymentExtensionGetter = Mockito.mock(PaymentExtensionGetter.class);
-        Mockito.when(paymentExtensionGetter.getPaymentExtensions())
-            .thenReturn(List.of(new WeChatPayment()));
-        PaymentRegistry registry = new PaymentRegistry(paymentExtensionGetter);
+        PaymentRegistry registry = new PaymentRegistry(getter);
 
         ReactiveExtensionClient client = new ReactiveExtensionClientTest();
         accountService = new AccountServiceImpl(registry, client);
@@ -43,11 +38,4 @@ class AccountServiceTest {
             .verifyComplete();
     }
 
-    Account createMockAccount() {
-        Account account = new Account();
-        account.setName("test");
-        account.setChannel("WeChat");
-        account.setMaster(true);
-        return account;
-    }
 }
---
+++ src/test/java/net/nanxu/payment/service/impl/CallbackServiceImplTest.java
@@ -0,0 +1,13 @@
+package net.nanxu.payment.service.impl;
+
+import net.nanxu.payment.service.CallbackService;
+import org.junit.jupiter.api.Test;
+
+class CallbackServiceImplTest {
+
+    CallbackService service;
+
+    @Test
+    void callback() {
+    }
+}
